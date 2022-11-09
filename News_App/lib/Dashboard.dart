f9cd4e3b41242d14037b0c9c7805c8b6bf7fbe59
Prajjwal Veer Basnet <prajjwalveerbasnet10@gmail.com>
2022-11-09 12:01:42 +0545

    News API Changes

--- News_App/lib/Dashboard.dart
+++ News_App/lib/Dashboard.dart
@@ -1,7 +1,9 @@
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/api/get.dart';
 import 'package:news_app/customcards.dart';
+import 'package:news_app/model/newsapi.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -13,6 +15,16 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   String url = 'https://e3.365dm.com/22/08/768x432/skynews-breaking-news-breakingnews_5875724.jpg?20220906151704';
   String imageURL = 'https://yt3.ggpht.com/MRywaef1JLriHf-MUivy7-WAoVAL4sB7VHZXgmprXtmpOlN73I4wBhjjWdkZNFyJNiUP6MHm1w=s900-c-k-c0x00ffffff-no-rj';
+  late Future<NewsApi?> futureNews;
+
+
+
+  @override
+  void initState(){
+    super.initState();
+    futureNews = GetApi.getNewsData(context) as Future<NewsApi>;
+
+  }
   @override
     Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -32,76 +44,105 @@ class _DashboardState extends State<Dashboard> {
 
 
       ),
-      body: Container(
+      body: SizedBox(
         width: size.width,
         child: Column(
           children: [
-            // horizontal listview
-            Container(
-              height: size.height/5,
-              child: ListView.builder(
-                primary: true,
-                  physics: const ClampingScrollPhysics(),
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index)
-                  => Card(
-                    elevation: 5,
-                    child: Container(
-                      child: Stack(
-                       children: [
-                         CachedNetworkImage(imageUrl: imageURL,
-                           fit: BoxFit.cover,
-                           width: size.width/1.6,
-                           height: size.height/5 ,
-                           errorWidget: (context, url, error)=>
-                           const Icon(Icons.do_not_disturb_alt,
-                             color: Colors.white,),
-                           placeholder: (context,url)=>
-                           const CircularProgressIndicator(),
-                         ),
-                         Positioned(
-                           bottom: 5,left: 10,
-                           child: Column(
-                             children: const [
-                               Padding(
-                                 padding: EdgeInsets.only(bottom: 0.0),
-                                 child: Text("Henlo Henlo",
-                                   style: TextStyle(fontSize: 15,
-                                       fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-                                 ),
-                               ),
-                               Text(" Date",
-                                 style: TextStyle(fontSize: 15,
-                                     fontStyle: FontStyle.italic),
-                               ),
-                             ],
-                           ),
-                         ),
-                         const Positioned(
-                           bottom: 10,
-                           right: 10,
-                           child: Icon(Icons.play_circle,
-                             color: Colors.white,size:30),
-                         ),
-                       ],
+            FutureBuilder(future: futureNews,
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              switch (snapshot.connectionState){
+                case ConnectionState.none:
+                  return Container(
+                    child: const Text("Try Loading Again"),
+                  );
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return Container(
+                    child: const CircularProgressIndicator(),
+                  );
 
-                      ),
-                    ),
-                  ),
-              ),
-            ),
-            Container(
-              height: size.height/1.5,
-              child: ListView.builder(
-                primary: true,
-                physics: const ClampingScrollPhysics(),
-                itemCount: 10,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index)
-                => VerticalListCard()
-              ),
-            ),
+                case ConnectionState.done:
+                  if(!snapshot.hasData){
+                    // no data
+                    return Container(
+                      child: const Text("No new data available"),
+                    );
+                  }
+                  else{
+                    // has data
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: size.height/5,
+                          child: ListView.builder(
+                            primary: true,
+                            physics: const ClampingScrollPhysics(),
+                            itemCount: snapshot.data.articles.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (BuildContext context, int index)
+                            => Card(
+                              elevation: 5,
+                              child: Container(
+                                child: Stack(
+                                  children: [
+                                    CachedNetworkImage(imageUrl: imageURL,
+                                      fit: BoxFit.cover,
+                                      width: size.width/1.6,
+                                      height: size.height/5 ,
+                                      errorWidget: (context, url, error)=>
+                                      const Icon(Icons.do_not_disturb_alt,
+                                        color: Colors.white,),
+                                      placeholder: (context,url)=>
+                                      const CircularProgressIndicator(),
+                                    ),
+                                    Positioned(
+                                      bottom: 5,left: 10,
+                                      child: Column(
+                                        children:[
+                                          Padding(
+                                            padding: const EdgeInsets.only(bottom: 0.0),
+                                            child: Text(snapshot.data.articles[index].title,
+                                              style: const TextStyle(fontSize: 15,
+                                                  fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                          Text(snapshot.data.articles[index].publishedAt,
+                                            style: const TextStyle(fontSize: 15,
+                                                fontStyle: FontStyle.italic),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const Positioned(
+                                      bottom: 10,
+                                      right: 10,
+                                      child: Icon(Icons.play_circle,
+                                          color: Colors.white,size:30),
+                                    ),
+                                  ],
+
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height/1.5,
+                          child: ListView.builder(
+                              primary: true,
+                              physics: const ClampingScrollPhysics(),
+                              itemCount: snapshot.data.articles.length,
+                              scrollDirection: Axis.vertical,
+                              itemBuilder: (BuildContext context, int index)
+                              => VerticalListCard(snapshot.data.articles[index]),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+              }
+            }),
+            // horizontal listview
           ],
         ),
       ),
--- News_App/lib/customcards.dart
+++ News_App/lib/customcards.dart
@@ -1,14 +1,18 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/model/newsapi.dart';
 
 class VerticalListCard extends StatelessWidget{
+  Articles articles;
+   VerticalListCard(this.articles);
+
   @override
   Widget build(BuildContext context) {
-    String imageURL = 'https://yt3.ggpht.com/MRywaef1JLriHf-MUivy7-WAoVAL4sB7VHZXgmprXtmpOlN73I4wBhjjWdkZNFyJNiUP6MHm1w=s900-c-k-c0x00ffffff-no-rj';
+    String imageURL = articles.urlToImage!;
    return Card(
      elevation: 5,
-     child: Container(
+     child: SizedBox(
        height: 100,
          child: Row(
 
@@ -41,8 +45,8 @@ class VerticalListCard extends StatelessWidget{
                child: Column(
                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                  children:[
-                   const Center(
-                     child: Text("News News News", style: TextStyle(color: Colors.black,
+                   Center(
+                     child: Text(articles.title!, style: const TextStyle(color: Colors.black,
                          fontSize: 16, fontWeight: FontWeight.bold),
                      ),
                    ),
@@ -54,13 +58,23 @@ class VerticalListCard extends StatelessWidget{
                            color: Colors.redAccent,
                            borderRadius: BorderRadius.circular(25),
                          ),
-                         child: const Text("Read More"),
+                         child: Text(articles.url!),
                        ),
                      ],
                    ),
-                 ],
+                   Row(
+                     children:[
+                         Padding(
+                           padding: EdgeInsets.only(left: 10.0),
+                           child: Text(articles.publishedAt!, style: const TextStyle(color: Colors.black,
+                           fontSize: 16, fontWeight: FontWeight.bold),
+                           ),
+                         )
+                         ],
+                       ),
+                     ],
+                  ),
                ),
-             ),
            ],
          ),
        ),
